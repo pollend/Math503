bb4fd0c6834ad5d4b713a47d67a1ea47d4088b58
Michael Pollind <mpollind@gmail.com>
2017-12-04 18:13:33 -0800

    test

--- data.R
+++ data.R
@@ -1,4 +1,8 @@
 library(readxl)
+library(sqldf)
+library(tidyr)
 
-seattle_temp_pop_data <- read_excel("~/project/Math503-Final/seattle temp & pop data.xlsx")
+temp_pop <- read_excel("~/project/Math503-Final/seattle temp & pop data.xlsx")
+crime_data <- read_excel("~/project/Math503-Final/seattle crime data.xlsx")
 
+crime_final <- separate(crime_data,"Date (monthly)",c("Year","Month","Day"), sep = "-")
